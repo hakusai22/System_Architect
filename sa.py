ae0abd4cdf9adc4a240181649e8ab8f418a8e3bf
yinpeng <y960303802@163.com>
2023-10-11 13:56:56 +0800

    [add] py3

---
+++ sa.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right, insort, bisect
+from math import ceil, floor, pow, gcd, sqrt, log10, fabs, fmod, factorial, inf, pi, e
+from heapq import heapify, heapreplace, heappush, heappop, heappushpop, nlargest, nsmallest
+from collections import defaultdict, Counter, deque
+from itertools import permutations, combinations, combinations_with_replacement, accumulate, count, groupby
+from queue import PriorityQueue, Queue, LifoQueue
+from functools import lru_cache
+from typing import List
+import sys
+
+sys.setrecursionlimit(10001000)
+
+MOD = int(1e9 + 7)
+INF = int(1e20)
+INFMIN = float('-inf')
+INFMAX = float('inf')
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+
+def alp(i):
+    return chr(ord('a') + i % 26)  # i=0->'a', i=25->'z'
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def end(r=-1):
+    print(r)
+    exit()
+
+# -*- coding: utf-8 -*-
+# @Author  : hakusai
+# @Time    : 2023/10/11 13:56
+
+if __name__ == '__main__':
+    print("系统架构师")
